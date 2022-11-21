a45c16bed15f7ef6fe226d5ab00c691464cd5c44
Roman Kuznetsov <kuznetsov.roman.orenburg@gmail.com>
2022-11-22 02:54:09 +0300

    funktion: statistiken CRUDs

--- application/nosql2h22-estate-backend/src/routes/index.js
+++ application/nosql2h22-estate-backend/src/routes/index.js
@@ -1,7 +1,9 @@
 import express from "express";
 
 import { housesRoutes } from "./houses.js";
+import { statsRoutes } from "./stats.js";
 
 export const routes = express.Router();
 
 routes.use("/houses", housesRoutes);
+routes.use("/stats", statsRoutes)
---
+++ application/nosql2h22-estate-backend/src/routes/stats.js
@@ -0,0 +1,68 @@
+import express from "express";
+import _ from "lodash";
+
+import { getDb } from "../db.js";
+import { logger } from "../logger.js";
+import { respondSuccess } from "../utils.js";
+
+export const statsRoutes = express.Router();
+
+statsRoutes.get("/area", async (req, res) => {
+    const district = req.query.district;
+    const dbConnection = getDb();
+
+    logger.info(`GET /stats/area, district=${district}`);
+
+    dbConnection
+        .collection("houses")
+        .find({district})
+        .project({areaHouse: 1, _id: 0})
+        .toArray()
+        .then((houses) => houses.reduce((sum, house) => sum + house.areaHouse, 0))
+        .then((area) => respondSuccess(res, area))
+        .catch((err) => respondError(res, err));
+});
+
+statsRoutes.get("/flats", async (req, res) => {
+    const district = req.query.district;
+    const dbConnection = getDb();
+
+    logger.info(`GET /stats/flats, district=${district}`);
+
+    dbConnection
+        .collection("houses")
+        .find({district})
+        .project({flat: 1, _id: 0})
+        .toArray()
+        .then((houses) => houses.map(house => house.flat))
+        .then((arr) => _.flatten(arr))
+        .then((flats) => flats.map(flat => flat.count))
+        .then((flatCounts) => flatCounts.reduce((sum, v) => sum + v, 0))
+        .then((flatCount) => respondSuccess(res, flatCount))
+        .catch((err) => respondError(res, err));
+});
+
+statsRoutes.get("/vacant", async (req, res) => {
+    const district = req.query.district;
+    const dbConnection = getDb();
+
+    logger.info(`GET /stats/vacant, district=${district}`);
+
+    throw Error("Not implemented!");
+});
+
+statsRoutes.get("/residents", async (req, res) => {
+    const district = req.query.district;
+    const dbConnection = getDb();
+
+    logger.info(`GET /stats/residents, district=${district}`);
+
+    dbConnection
+        .collection("houses")
+        .find({district})
+        .project({countLiving: 1, _id: 0})
+        .toArray()
+        .then((houses) => houses.reduce((sum, house) => sum + house.countLiving, 0))
+        .then((area) => respondSuccess(res, area))
+        .catch((err) => respondError(res, err));
+});
