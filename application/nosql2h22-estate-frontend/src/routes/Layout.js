f7ee671adfd456bdd7f3d6b37f3600b2f8eb2ae4
PavelKovalev1444 <pavel.kovalev30@gmail.com>
2022-11-21 23:15:30 +0300

    man hat ein bootstrap hinzugefuegt

--- application/nosql2h22-estate-frontend/src/routes/Layout.js
+++ application/nosql2h22-estate-frontend/src/routes/Layout.js
@@ -6,16 +6,24 @@ import {
   Outlet
 } from "react-router-dom";
 
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 const Layout = () => {
   return (
-    <div>
-      <div className="sidebar">
-        <Sidebar />
-      </div>
-      <div className="content">
-        <Outlet/>
-      </div>
-    </div>  
+    <Container fluid>
+      <Row md={4}>
+        <Col xs={6} md={4}>
+          <div className="sidebar">
+            <Sidebar />
+          </div>
+        </Col>
+        <Col xs={12} md={8}>
+          <Outlet/>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
