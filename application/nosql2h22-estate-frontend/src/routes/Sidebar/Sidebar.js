911f317263c9327654b623fbbf4292cf40f1816f
PavelKovalev1444 <pavel.kovalev30@gmail.com>
2022-11-21 15:44:13 +0300

    man hat Balken hinzugefuegt

--- application/nosql2h22-estate-frontend/src/routes/Sidebar/Sidebar.js
+++ application/nosql2h22-estate-frontend/src/routes/Sidebar/Sidebar.js
@@ -1,14 +1,8 @@
 import React from 'react'
 import './../../styles/Sidebar.css'
 import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
   Link
 } from "react-router-dom";
-
-import Cathalog from './../../components/pages/Cathalog.js'
-import Map from './../../components/pages/Map.js'
 
 const Sidebar = () => {
 
