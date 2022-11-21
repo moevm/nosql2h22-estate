545efa642654bedebc284d2bedf5540962a768c5
Roman Kuznetsov <kuznetsov.roman.orenburg@gmail.com>
2022-11-22 01:59:16 +0300

    funktion: Katalog- und Karten-CRUDs

--- application/nosql2h22-estate-backend/src/routes.js
+++ application/nosql2h22-estate-backend/src/routes.js
@@ -1,7 +1,8 @@
 import _ from "lodash";
+import { ObjectId } from "mongodb";
 import express from "express";
 
-import { respondSuccess, respondError } from "./utils.js";
+import { respondSuccess, respondError, shortProjection } from "./utils.js";
 import { getDb } from "./db.js";
 import { scheme } from "./houseScheme.js";
 import { isValid } from "./validation.js";
@@ -9,8 +10,50 @@ import { logger } from "./logger.js";
 
 export const routes = express.Router();
 
+routes.get("/houses/short", async (req, res) => {
+  const dbConnection = getDb();
+
+  logger.info("GET /houses/short, no page specified");
+
+  dbConnection
+    .collection("houses")
+    .find()
+    .project(shortProjection)
+    .toArray()
+    .then(houses => respondSuccess(res, houses))
+    .catch(err => respondError(res, err))
+});
+
+routes.get("/houses/:id", async (req, res) => {
+  const dbConnection = getDb();
+
+  logger.info(`GET /houses/:id, id=${req.params.id}`)
+
+  dbConnection
+    .collection("houses")
+    .findOne(ObjectId(req.params.id))
+    .then(house => respondSuccess(res, house))
+    .catch(err => respondError(res, err))
+});
+
+routes.get("/houses", async (req, res) => {
+  const dbConnection = getDb();
+
+  logger.info("GET /houses, no page specified");
+
+  dbConnection
+    .collection("houses")
+    .find()
+    .toArray()
+    .then(houses => respondSuccess(res, houses))
+    .catch(err => respondError(res, err))
+});
+
 routes.post("/houses", async (req, res) => {
   const dbConnection = getDb();
+
+  logger.info(`POST /houses, data=${req.body}`);
+
   const house = _.pick(
     req.body,
     scheme.map((prop) => prop.name)
@@ -18,7 +61,7 @@ routes.post("/houses", async (req, res) => {
   const validationResult = isValid(req.body, scheme);
 
   if (validationResult.valid) {
-    logger.info("Got valid response, sending request to db");
+    logger.info(`POST /houses, house validated`);
 
     dbConnection
       .collection("houses")
--- application/nosql2h22-estate-backend/src/utils.js
+++ application/nosql2h22-estate-backend/src/utils.js
@@ -1,11 +1,13 @@
 import { logger } from "./logger.js";
 
-export const respondError = (res, err) => {
-  res.status(400).json({ status: "error", error: err });
+export const respondError = (res, error) => {
+  res.status(400).json({ status: "error", error });
   logger.error("Request error: ", err);
 };
 
-export const respondSuccess = (res) => {
-  res.status(200).json({ status: "done" });
-  logger.info("Request success");
+export const respondSuccess = (res, message) => {
+  res.status(200).json({ status: "done", message });
+  logger.info("Request success: ", message);
 };
+
+export const shortProjection = ["street", "houseNumber", "houseFractionNumber", "character", "district"].reduce((res, k) => _.set(res, k, 1), {});
