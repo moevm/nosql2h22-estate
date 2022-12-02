8ef858cae7a15b11cf502abd8a8f16c28dc9f414
PavelKovalev1444 <pavel.kovalev30@gmail.com>
2022-12-02 14:40:47 +0300

    man hat fehler hinzugefuegt

--- application/nosql2h22-estate-frontend/src/components/pages/Authorization.js
+++ application/nosql2h22-estate-frontend/src/components/pages/Authorization.js
@@ -1,16 +1,71 @@
-import React from 'react'
+import React, { useState } from 'react'
 //components
+
+//icons
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faExclamationTriangle } from '@fortawesome/free-solid-svg-icons'
 
 //styles
 import './../../styles/Authorization.css'
 
 const Authorization = () => {
   
+  const [errorWithAuthorization, setErrorWithAuthorization] = useState(false)
+
   const handleAuthorizationForm = (e) => {
     e.preventDefault();
 
     console.log('authorization')
+    setErrorWithAuthorization(!errorWithAuthorization)
+  }
 
+  function formBody() {
+    console.log(errorWithAuthorization)
+    if (errorWithAuthorization){
+      return (
+        <div className="authorization-card-form">
+          <div className="authorization-card-form-header-container">
+            <span className="authorization-card-form-header-text-error">
+              Ключ администратора
+            </span>
+          </div>
+          <div className="authorization-card-form-input-container">
+            <input className="authorization-card-form-input-error" type="password"/>
+          </div>
+          <div className="authorization-card-form-err-msg-container">
+            <FontAwesomeIcon icon={faExclamationTriangle} className="authoriation-card-form-icon"/>
+            <span className="authorization-card-form-err-msg">
+              Неверный ключ администратора
+            </span>
+          </div>
+          <div className="authorization-div-line"></div>
+          <button className="authorization-card-form-button" type="submit">
+            <span className="authorization-card-form-button-text">
+              Вход
+            </span>
+          </button>
+        </div>
+      ) 
+    }else{
+      return (
+        <div className="authorization-card-form">
+          <div className="authorization-card-form-header-container">
+            <span className="authorization-card-form-header-text">
+              Ключ администратора
+            </span>
+          </div>
+          <div className="authorization-card-form-input-container">
+            <input className="authorization-card-form-input" type="password"/>
+          </div>
+          <div className="authorization-div-line"></div>
+          <button className="authorization-card-form-button" type="submit">
+            <span className="authorization-card-form-button-text">
+              Вход
+            </span>
+          </button>
+        </div>
+      )
+    }
   }
 
   return (
@@ -30,22 +85,7 @@ const Authorization = () => {
             Вход
           </span>
         </div>
-        <div className="authorization-card-form">
-          <div className="authorization-card-form-header-container">
-            <span className="authorization-card-form-header-text">
-              Ключ администратора
-            </span>
-          </div>
-          <div className="authorization-card-form-input-container">
-            <input className="authorization-card-form-input" type="password"/>
-          </div>
-          <div className="authorization-div-line"></div>
-          <button className="authorization-card-form-button" type="submit">
-            <span className="authorization-card-form-button-text">
-              Вход
-            </span>
-          </button>
-        </div>
+        { formBody() }
       </form>  
     </div>
   );
