8166b32c687dd27666dc8eb054a7111e11f40829
PavelKovalev1444 <pavel.kovalev30@gmail.com>
2022-12-02 14:13:55 +0300

    man hat Seitenlayout erstellt

--- application/nosql2h22-estate-frontend/src/components/pages/Authorization.js
+++ application/nosql2h22-estate-frontend/src/components/pages/Authorization.js
@@ -1,11 +1,52 @@
 import React from 'react'
+//components
+
+//styles
 import './../../styles/Authorization.css'
 
 const Authorization = () => {
   
+  const handleAuthorizationForm = (e) => {
+    e.preventDefault();
+
+    console.log('authorization')
+
+  }
+
   return (
-    <div>
-      <h1>Authorization</h1>
+    <div className="authorization-layout">
+
+      <div className="authorization-header">
+        <div className="authorization-header-text-container">
+          <span className="authorization-header-text">
+            Авторизация
+          </span>
+        </div>
+      </div>
+
+      <form className="authorization-card" onSubmit={handleAuthorizationForm}>
+        <div className="authorization-card-header-container">
+          <span className="authorization-card-header-text">
+            Вход
+          </span>
+        </div>
+        <div className="authorization-card-form">
+          <div className="authorization-card-form-header-container">
+            <span className="authorization-card-form-header-text">
+              Ключ администратора
+            </span>
+          </div>
+          <div className="authorization-card-form-input-container">
+            <input className="authorization-card-form-input" type="password"/>
+          </div>
+          <div className="authorization-div-line"></div>
+          <button className="authorization-card-form-button" type="submit">
+            <span className="authorization-card-form-button-text">
+              Вход
+            </span>
+          </button>
+        </div>
+      </form>  
     </div>
   );
 
