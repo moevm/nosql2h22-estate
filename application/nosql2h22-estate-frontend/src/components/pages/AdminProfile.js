b6b2c665c830bdcd50ebddeb6b40b9f7db04efd1
maikleProko <maiklProko@gmail.com>
2022-12-05 22:55:38 +0300

    updated

---
+++ application/nosql2h22-estate-frontend/src/components/pages/AdminProfile.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Navbar from "../../routes/Navbar/Navbar";
+import AdminProfileComponent from "./AdminProfile/AdminProfileComponent";
+//import './../../styles/AdminProfile.css'
+
+const AdminProfile = () => {
+
+    return (
+        <div>
+            <div className="navbar">
+                <Navbar headerText={"Профиль администратора"} descText={"Ниже представлен ваш профиль"}/>
+            </div>
+            <div className="AdminProfile-component">
+                <AdminProfileComponent/>
+            </div>
+        </div>
+    );
+
+};
+
+export default AdminProfile;
